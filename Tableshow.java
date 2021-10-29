3b510b4a97b334f61d541e00182a64c5034d57f3
snejkalol <37933334+snejkalol@users.noreply.github.com>
2021-10-29 16:24:43 +0300

    Update Tableshow.java

--- Tableshow.java
+++ Tableshow.java
@@ -3,6 +3,6 @@ import com.jakewharton.fliptables.FlipTable;
 
 public class Tableshow {
     public static void ts(String [] headers, String[][] data) {
-                System.out.println(FlipTable.of(headers, data));  //вывод таблицы, пусть пока тут полежит
+                System.out.println(FlipTable.of(headers, data));
     }
 }
